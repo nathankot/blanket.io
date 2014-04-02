fd6762e550545ae72e84cee419b09ba3347da59c
Nathan Kot <me@nathankot.com>
2014-04-02 22:37:19 +1300

    Support dev env compiling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    ngmin = require('gulp-ngmin'),
+    uglify = require('gulp-uglify');
 
 var ENV = require('minimist')(process.argv.slice(2)).env || 'development';
 
 gulp.task('scripts', function() {
-  gulp.src('./web/src/js/app.js')
+  var proc = gulp.src('./web/src/js/app.js')
     .pipe(browserify({
       insertGlobals: true,
       transform: ['debowerify'],
       debug: ENV === 'development'
-    }))
-    .pipe(gulp.dest('./web/dist/js'));
+    }));
+
+  if (ENV !== 'development') {
+    proc.pipe(ngmin())
+        .pipe(uglify());
+  }
+
+  proc.pipe(gulp.dest('./web/dist/js'));
 });
--- web/src/js/app.js
+++ web/src/js/app.js
@@ -1,1 +1,17 @@
 var angular = require('angular');
+require('angular-ui-router');
+
+angular.module('rssApp', ['ui.router'])
+       .config([
+         '$locationProvider',
+         '$stateProvider',
+         '$urlRouterProvider',
+         function ($locationProvider, $stateProvider, $urlRouterProvider) {
+          $locationProvider.html5Mode(true);
+          $urlRouterProvider.otherwise('/');
+
+          $stateProvider.state('home', {
+            url: '/',
+            templateUrl: 'views/landing.html'
+          });
+       }]);
