d18f8fc1954189ddd12659249f80a302c697ab0a
Nathan Kot <me@nathankot.com>
2014-04-03 21:22:22 +1300

    Basic gulp as build tool.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,81 @@
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     ngmin = require('gulp-ngmin'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    stylus = require('gulp-stylus'),
+    csso = require('gulp-csso'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    rev = require('gulp-rev-all'),
+    clean = require('gulp-clean'),
+    Q = require('q'),
 
-var ENV = require('minimist')(process.argv.slice(2)).env || 'development';
+    ENV,
+    BUILD_PATH;
 
 gulp.task('scripts', function() {
-  var proc = gulp.src('./web/src/js/app.js')
-    .pipe(browserify({
-      insertGlobals: true,
-      transform: ['debowerify'],
-      debug: ENV === 'development'
-    }));
+  var proc = gulp.src('web/src/js/app.js')
+                .pipe(browserify({
+                  insertGlobals: true,
+                  transform: ['debowerify'],
+                  debug: ENV === 'development'
+                }));
 
   if (ENV !== 'development') {
-    proc.pipe(ngmin())
-        .pipe(uglify());
+    proc.pipe(ngmin()).pipe(uglify());
   }
 
-  proc.pipe(gulp.dest('./web/dist/js'));
+  proc.pipe(gulp.dest(BUILD_PATH + '/js'));
 });
+
+gulp.task('styles', function() {
+  gulp.src('web/src/styl/*.styl')
+      .pipe(stylus({
+        paths: ['web/components/'],
+        use: ['nib'],
+        import: ['nib'],
+        set: ['linenos']
+      }))
+      .pipe(csso(ENV === 'development'))
+      .pipe(gulp.dest(BUILD_PATH + '/css'));
+});
+
+gulp.task('views', function() {
+  gulp.src('web/src/views/**/*.html')
+      .pipe(gulp.dest(BUILD_PATH + '/views'));
+});
+
+gulp.task('images', function() {
+  gulp.src('web/src/images/**/*.(png|jpg|gif|svg)')
+      .pipe(imagemin())
+      .pipe(gulp.dest(BUILD_PATH + '/images'));
+});
+
+gulp.task('setProduction', function() {
+  ENV = 'production';
+  BUILD_PATH = 'web/dist';
+});
+
+gulp.task('setDevelopment', function() {
+  ENV = 'development';
+  BUILD_PATH = 'web/dev';
+});
+
+gulp.task('rev', function() {
+  gulp.src(BUILD_PATH + '/**', { base: BUILD_PATH })
+      .pipe(rev())
+      .pipe(gulp.dest(BUILD_PATH));
+});
+
+gulp.task('clean', function() {
+  gulp.src([
+    BUILD_PATH + '/js/**',
+    BUILD_PATH + '/css/**',
+    BUILD_PATH + '/views/**',
+    BUILD_PATH + '/images/**'
+  ], { read: false  }).pipe(clean());
+});
+
+gulp.task('all', ['scripts', 'styles', 'views', 'images']);
+gulp.task('dev', ['setDevelopment', 'clean', 'scripts', 'styles', 'views', 'images']);
+gulp.task('dist', ['setProduction', 'clean', 'scripts', 'styles', 'views', 'images', 'rev']);
