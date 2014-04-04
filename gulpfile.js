e1ba3e9617a14b313bec3b2d86e1dc4d94fe1085
Nathan Kot <me@nathankot.com>
2014-04-05 00:39:16 +1300

    Use the min reporter.

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task('server', function() {
 gulp.task('test', function() {
   gulp.src('spec/**/*Spec.js')
       .pipe(mocha({
-        reporter: 'nyan',
+        reporter: 'min',
         bail: true
       }));
 });
