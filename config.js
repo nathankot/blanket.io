6b5a0def45cea1bdd7b0926adf9aef52240714a7
Nathan Kot <me@nathankot.com>
2014-04-05 11:36:45 +1300

    Setup mongourl properly

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var config = {
-  MONGO_URL: process.env.MONGO_URL ||
-             process.env.MONGOHQ_URL ||
-             'localhost'
+  MONGO_URL: (function() {
+    return process.env.MONGO_URL ||
+      process.env.MONGOHQ_URL ||
+      'mongodb://localhost/rss';
+  })()
 };
 
 module.exports = config;
