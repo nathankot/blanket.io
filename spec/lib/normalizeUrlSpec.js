b283581572135b970a5361dae0d30b3765cd0c4c
Nathan Kot <me@nathankot.com>
2014-04-05 21:39:57 +1300

    Normalize url - another case.

--- spec/lib/normalizeUrlSpec.js
+++ spec/lib/normalizeUrlSpec.js
@@ -2,17 +2,17 @@ var normalizeUrl = require('../../lib/normalizeUrl.js'),
     expect = require('chai').expect,
     _ = require('lodash');
 
-describe('Lib: normalizeUrl', function() {
-
+describe.only('Lib: normalizeUrl', function() {
   _.each({
     'news.ycombinator.com': 'http://news.ycombinator.com',
     'https://news.ycombinator.com/': 'https://news.ycombinator.com',
     'nzherald.com/blog/rss/?query=123': 'http://nzherald.com/blog/rss',
-    'http://arstechnica.com/?superman&rules': 'http://arstechnica.com'
+    'http://arstechnica.com/?superman&rules': 'http://arstechnica.com',
+    'news.ycombinator.com/': 'http://news.ycombinator.com',
+    '//news.ycombinator.com/': 'http://news.ycombinator.com'
   }, function(expected, test) {
     it('should normalize ' + test + ' to ' + expected, function() {
       expect(normalizeUrl(test)).to.equal(expected);
     });
   });
-
 });
