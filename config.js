d55d67d491cc1bb9492ea89e5e0a696b354d6764
Nathan Kot <me@nathankot.com>
2014-04-05 21:19:49 +1300

    Allow for test db.

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ var config = {
   MONGO_URL: (function() {
     return process.env.MONGO_URL ||
       process.env.MONGOHQ_URL ||
+      process.env.NODE_ENV === 'testing' ? 
+      'mongodb:/localhost/rss-test' :
       'mongodb://localhost/rss';
   })()
 };
