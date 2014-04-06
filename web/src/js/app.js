ce71a751e5d5312056b345f362560379e47b897f
Nathan Kot <me@nathankot.com>
2014-04-06 13:54:02 +1200

    Add and config restangular.

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -1,20 +1,30 @@
 require('angular');
 require('angular-ui-router');
+require('restangular');
 
-angular.module('rssApp', ['ui.router'])
-       .config([
-         '$locationProvider',
-         '$stateProvider',
-         '$urlRouterProvider',
-         function ($locationProvider, $stateProvider, $urlRouterProvider) {
-          $locationProvider.html5Mode(true);
-          $urlRouterProvider.otherwise('/');
+var app = angular.module('rssApp', [
+  'ui.router',
+  'restangular'
+]).config([
+  '$locationProvider',
+  '$stateProvider',
+  '$urlRouterProvider',
+  function (
+    $locationProvider, 
+    $stateProvider, 
+    $urlRouterProvider,
+    RestangularProvider
+  ) {
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
+    RestangularProvider.setRestangularFields({ id: '_id' });
 
-          $stateProvider.state('home', {
-            url: '/',
-            templateUrl: 'views/landing.html'
-          });
-       }]);
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'views/landing.html'
+    });
+}]);
 
 // Controllers
 require('./controllers/landing.js');
+require('./controllers/sources.js');
