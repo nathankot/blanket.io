8cb9ed7b984fe45bd6634649747075b1ec04a994
Nathan Kot <me@nathankot.com>
2014-04-06 12:47:20 +1200

    Item spec + implementation.

---
+++ models/item.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    normalizeUrl = require('../lib/normalizeUrl.js');
+
+module.exports = mongoose.model('Item', (function() {
+
+  var schema = mongoose.Schema({
+    _source: mongoose.Schema.Types.ObjectId,
+    url: { type: mongoose.SchemaTypes.Url, unique: true, required: true },
+    title: String,
+    summary: String,
+    occurrences: { type: Number, default: 1 },
+    views: Number,
+    topics: Array,
+    publishedAt: Date,
+    createdAt: { type: Date, default: Date.now }
+  });
+
+  schema.pre('validate', function(done) {
+    this.url = normalizeUrl(this.url);
+    done();
+  });
+
+  return schema;
+
+})());
---
+++ spec/models/itemSpec.js
@@ -0,0 +1,73 @@
+/* jshint expr: true */
+
+require('../../db.js');
+var Item = require('../../models/item.js'),
+    expect = require('chai').expect;
+
+describe.only('Model: Item', function() {
+
+  var item;
+
+  beforeEach(function(done) { Item.collection.remove(done); });
+
+  beforeEach(function(done) {
+    new Item({
+      url: 'http://nzherald.co.nz/nz/news/article.cfm/?c_id=1&objectid=11233247',
+      title: 'A man-made noice',
+      topics: ['flight', 'plane', 'malaysian'],
+      publishedAt: '2014-04-06 12:21:57 +1200',
+      summary: 'A signal that is "consistent with an aircraft black box" ' +
+               'is now the focus of the search for the missing Flight MH370, '+
+               'but authorities are warning there is no confirmed link.'
+    }).save(function(err, i) {
+      if (err) { console.log(err); }
+      item = i;
+      done();
+    });
+  });
+
+  it('should set occurences to 1 for new item', function() {
+    expect(item.occurrences).to.equal(1);
+  });
+
+  it('should normalize the url', function() {
+    expect(item.url).to.equal('http://nzherald.co.nz/nz/news/article.cfm?c_id=1&objectid=11233247');
+  });
+
+  describe('duplicate item', function() {
+
+    it('should err upon save', function(done) {
+      new Item({
+        url: 'http://nzherald.co.nz/nz/news/article.cfm/?c_id=1&objectid=11233247'
+      }).save(function(err) {
+        expect(err).to.be.instanceOf(Error);
+        done();
+      });
+    });
+
+  });
+
+  describe('item.toJSON()', function() {
+    var subject;
+    beforeEach(function() {
+      subject = item.toJSON();
+    });
+
+    it('should have url', function() {
+      expect(subject.url).not.to.be.an.undefined;
+    });
+
+    it('should have title', function() {
+      expect(subject.title).not.to.be.an.undefined;
+    });
+
+    it('should have summary', function() {
+      expect(subject.summary).not.to.be.an.undefined;
+    });
+
+    it('should have an array of topics', function() {
+      expect(subject.topics).to.be.an.array;
+    });
+  });
+
+});
