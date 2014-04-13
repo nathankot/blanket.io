e54f4d6d62cd6be0871ee557b4c4a4236c05c325
Nathan Kot <me@nathankot.com>
2014-04-13 16:52:47 +1200

    Seperate loggers.

--- app.js
+++ app.js
@@ -40,16 +40,17 @@ if (cluster.isMaster) {
 
   // all environments
   app.set('port', process.env.PORT || 3666);
-  app.use(express.logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded());
   app.use(express.methodOverride());
   app.use(app.router);
 
   if ('development' == app.get('env')) {
+    app.use(express.logger('dev'));
     app.use(express.errorHandler());
     app.use(express.static(path.join(__dirname, 'web', 'dev')));
   } else {
+    app.use(express.logger());
     app.use(errorHandler());
     app.use(express.static(path.join(__dirname, 'web', 'dist')));
   }
