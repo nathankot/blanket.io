be93079d322284195b8631a6f8789451531985eb
Nathan Kot <me@nathankot.com>
2015-05-30 15:26:33 +0900

    Preload all models

--- db.js
+++ db.js
@@ -6,3 +6,12 @@ var config = require('./config');
 require('./config/mongoose.js')();
 module.exports = mongoose.connect(config.MONGO_URL).connection;
 
+var path = require('path');
+var fs = require('fs');
+var models_path = path.join(__dirname, 'models');
+
+fs.readdirSync(models_path).forEach(function (file) {
+    if (file.match(/\.js$/)) {
+        require(models_path+'/'+file);
+    }
+});
